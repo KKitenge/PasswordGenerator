30cff5ca922cbe72a18e5a22be655d1a728b0eb7
Ox Noj <geneveva@gmail.com>
2023-03-30 14:06:21 -0700

    function criteria edited to successfully ask for and remind the range of required characters

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,57 @@
 // Assignment code here
 
 //Objective is to randomly generate a password that meets certain criteria
-    // 
+    
 
-//Click button to generate a password
+    //User input value unkown
+var UserInput;
+//var passwordlength = [8 - 128]; - this isn't working
 
-//Prompt user for criteria
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var special = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+var numeric = ["0123456789"]
 
-//Prompt for length between 8 and 128 characters
-var passwordLength = 
-prompt("Please choose a password between 8 and 128 characters in length. Enter the character count here: ")
+//validate criteria
 
 //Prompt for special characters-upper/lowercase and numeric - special characters from owasp.org
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var numeric = "0123456789";
+// Yes or no answers to be retained
+//var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+//var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//var special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//var numeric = "0123456789";
+
+var lowerCaseAnswer = 
+//var upperCaseAnswer =
+//var specialAnswer =
+//var numericAnswer =
+
+//
+
+//Prompt user for criteria
+var passwordCriteria = alert("Hello! \nTo generate your new password, please proceed with the following steps...");
+
+prompt("Please choose a password between 8 and 128 characters in length. Enter the character count below: ");
+
+//Prompt for length between 8 and 128 characters
+function criteria(){
+   // prompt("Please choose a password between 8 and 128 characters in length. Enter the character count below: ");
+    var passwordLength = parseInt(passwordLength);{
+    if (UserInput < 8){
+      alert("Please choose a character limit between 8 through 128");
+    }
+    else if (UserInput > 128)
+      alert("Please choose a character limit under 128");
+  }
+}
+
+
 
 
 //Each prompt is to be answered with validation
+function answerConfirm() {
+  confirm("Please confirm your answer by clicking on OK to continue");
+}
 
 //When all prompts are answered, a password is generated that matches all selected criteria
 
