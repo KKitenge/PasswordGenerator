1a319c55aabaa048b2bda040341f2ee5cbf3141b
Ox Noj <geneveva@gmail.com>
2023-03-30 22:03:01 -0700

    edited script.js to where generate button is connected to user input action

--- assets/script.js
+++ assets/script.js
@@ -1,69 +1,47 @@
 // Assignment code here
 
 //Objective is to randomly generate a password that meets certain criteria
-    
 
-    //User input value unkown
-var UserInput;
-//var passwordlength = [8 - 128]; - this isn't working
+var userInput;
+var userNum;
+var userChar;
+var userLow;
+var userUpper;
 
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var special = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
 var numeric = ["0123456789"]
 
-//validate criteria
+//Add event listener to generate button
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
 
-//Prompt for special characters-upper/lowercase and numeric - special characters from owasp.org
-// Yes or no answers to be retained
-//var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-//var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//var special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-//var numeric = "0123456789";
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+}
 
-var lowerCaseAnswer = 
-//var upperCaseAnswer =
-//var specialAnswer =
-//var numericAnswer =
-
-//
-
-//Prompt user for criteria
-//var passwordCriteria = alert("Hello! \nTo generate your new password, please proceed with the following steps...");
-
-//prompt("Please choose a password between 8 and 128 characters in length. Enter the character count below: ");
-
+//Prompt for user to input criteria
 //Prompt for length between 8 and 128 characters
 function generatePassword(){
   userInput = prompt("How many characters would you like your password? Choose between 8 and 128");    
     if (userInput < 8 || userInput > 128) {      
         userInput = prompt("Please choose a password only between 8 and 128 characters in length");
     } else {
-        // confirm() dialog box displayed with ok or cancel/true, false
-        userNumber = confirm("Will this contain numbers? - Ok for Yes and Cancel for No");
-        userCharacter = confirm("Will this contain special characters? - Ok for Yes and Cancel for No");
-        userUppercase = confirm("Will this contain Uppercase letters? - Ok for Yes and Cancel for No");
-        userLowercase = confirm("Will this contain Lowercase letters? - Ok for Yes and Cancel for No");
-    };
+        userNum = confirm("Will this contain numbers? - Ok for Yes and Cancel for No");
+        userChar = confirm("Will this contain $peci@! characters? - Ok for Yes and Cancel for No");
+        userUpper = confirm("Will this contain UPPERCASE letters? - Ok for Yes and Cancel for No");
+        userLower = confirm("Will this contain lowercase letters? - Ok for Yes and Cancel for No");
+    }
+
+
+
 }
 
 
 
-//When all prompts are answered, a password is generated that matches all selected criteria
-
-//The password is displayed on the page or in an alert
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generatePassword()
